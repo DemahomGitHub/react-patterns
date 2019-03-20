46d0b979175e8ee4805e1b75f2a71c5cab38fda6
Mohamed Touré <demahom.toure@gmail.com>
2019-03-20 17:43:53 +0100

    Small change
    This is a useless commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ReactContextBoard from "./components/ReactContextProvider";
 
 import "./styles.css";
 
 // 1. Function component
 function MyFunctionComponent() {
   return (
-    <div className="Card">
+    <div className="Card Red-Card">
       <h3>React Patterns tutorial </h3>
       <div>This card is a function component</div>
     </div>
@@ -16,7 +17,7 @@ function MyFunctionComponent() {
 // 2. Destructing props
 function DestructingProps(props) {
   let { message } = props;
-  return <div className="Card">{message}</div>;
+  return <div className="Card Yellow-Card">{message}</div>;
 }
 DestructingProps.defaultProps = {
   message: "Destructing props default message"
@@ -33,7 +34,7 @@ function MergeDestructedProposWithOtherValues({ className, ...props }) {
   let { text } = { ...props };
   let classNames = ["btn", className].join(" ");
   return (
-    <div className="Card">
+    <div className="Card Green-Card">
       <h5>Merge destructed with other values</h5>
       <button className={classNames}>{text}</button>
     </div>
@@ -46,13 +47,13 @@ function MergeDestructedProposWithOtherValues({ className, ...props }) {
 // IF-ELSE statement: condition ? <span>Rendered when condiontion = true </span> : <span>Rendered when condiontion = false </span>
 function ConditionalRendering({ name, age }) {
   return (
-    <div className="Card">
+    <div className="Card Blue-Card">
       <h5>Conditional Rendering </h5>
-      {age < 13 && <span>Hey {name}! You're still a child. </span>}
+      {age < 13 && <span>Hey {name}! You are still a child. </span>}
       {age < 18 ? (
-        <span>Hey {name}! You're still a Teenager. </span>
+        <span>Hey {name}! You are still a Teenager. </span>
       ) : (
-        <span>Hey {name}! You're an adult. </span>
+        <span>Hey {name}! You are an adult. </span>
       )}
     </div>
   );
@@ -61,14 +62,16 @@ function ConditionalRendering({ name, age }) {
 // 6. Children Types
 // React can render children String or Array.
 function ChildrenAsTypes() {
-  return <div className="Card">This is a String as a child type</div>;
+  return (
+    <div className="Card Yellow-Card">This is a String as a child type</div>
+  );
 }
 
 // 7. Array as children
 function ArrayAsChildren() {
   let arr = ["Banana", "Orange", "Pineapple"];
   return (
-    <div className="Card">
+    <div className="Card Purple-Card">
       <h5>My favorite fruits</h5>
       {arr.map((item, i) => (
         <li key={i}>{item}</li>
@@ -84,7 +87,7 @@ function ArrayAsChildren() {
 function RenderPropComponent(props) {
   const UserProfile = ({ children }) => children(props);
   return (
-    <div className="Card">
+    <div className="Card LightBlue-Card">
       <h5>Render prop component </h5>
       <UserProfile>
         {({ name, sex, age, revenu }) => (
@@ -103,9 +106,41 @@ function RenderPropComponent(props) {
   );
 }
 
-// 10. Children pass-through
+// 10. Children pass-through - React Context (an alternative to React-Redux)
+// The React Context Provider implements this pattern. Have a look at ReactContextProvider.js file
+
+// Layout component
+
+// Container Component
+
+// Higher-order component
+const DatatableBuilder = (Header, Body, Footer) =>
+  class extends React.Component {
+    render() {
+      return (
+        <div className="Card">
+          <Header {...this.props} />
+          <Body {...this.props} />
+          <Footer {...this.props} />
+        </div>
+      );
+    }
+  };
+
+const Header1 = ({ title }) => {
+  return <h2 className="Card Red-Card"> {title} </h2>;
+};
+
+function Body1({ content }) {
+  return <div className="Card Yellow-Card">{content}</div>;
+}
+
+function Footer1({ info }) {
+  return <div className="Card Green-Card">{info}</div>;
+}
 
 function App() {
+  const DataTable = DatatableBuilder(Header1, Body1, Footer1);
   return (
     <div>
       <MyFunctionComponent />
@@ -119,7 +154,13 @@ function App() {
       <ConditionalRendering name="John" age="25" />
       <ChildrenAsTypes />
       <ArrayAsChildren />
-      <RenderPropComponent name="The Riche Guy" sex="M" age="32" revenu="1M" />
+      <RenderPropComponent name="The Rich Guy" sex="M" age="15" revenu="1M" />
+      <ReactContextBoard />
+      <DataTable
+        title="This is a header"
+        content="This the body of the function"
+        info="Hello INFO"
+      />
     </div>
   );
 }
